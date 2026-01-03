8bd0b3a9249cf9723f0477435f3c79bfb77172b2
Gareth Chester <gareth.chester@dunelm.org.uk>
2026-01-03 14:18:15 +0000

    Rename and add main.js

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const initGis = () => {
+    const cachedToken = localStorage.getItem('bq_token');
+    const tokenTime = localStorage.getItem('bq_token_time');
+    const oneHour = 3600 * 1000;
+    tokenClient = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: 'https://www.googleapis.com/auth/bigquery.readonly',
+        callback: (resp) => {
+            if (resp.access_token) {
+                localStorage.setItem('bq_token', resp.access_token);
+                localStorage.setItem('bq_token_time', Date.now())
+                isLoggedIn.value = true;
+                executeTemplate();
+            } else if (resp.error === 'interaction_required') {
+                // This is expected if the user needs to click a button
+                console.log("Silent auth failed: User interaction required.");
+                isLoggedIn.value = false;
+            }
+        },
+    });
+    console.log(cachedToken)
+    console.log(Date.now())
+    console.log(tokenTime)
+    if (cachedToken && (Date.now() - tokenTime < oneHour)) {
+        // Token is still fresh! No popup needed.
+        console.log('win')
+        isLoggedIn.value = true;
+        executeTemplate();
+    } else {
+        // Token is missing or expired. 
+        // We MUST ask the user to click the button or try one silent check.
+        tokenClient.requestAccessToken({ prompt: 'none' });
+    }
+};
